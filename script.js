1df3a11752e389c2318488321739b47f3162e8fd
miguel bastidas <m.alejandro_80@hotmail.com>
2020-06-18 14:08:39 -0400

    changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /** @format */
 
-$('body').load('http://malejand80.github.io/chat');
+$('body').load('https://github.com/malejandro80/chat/blob/gh-pages/index.html');
 
 $('head').html(
   '<link rel="stylesheet" href="https://denyncrawford.github.io/main.css">'
